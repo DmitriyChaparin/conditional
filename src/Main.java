a1ead616053fb9d33adc13e31e869389a0c198ce
User <333@fd.com>
2022-09-27 19:44:57 +0400

    Домашнее задание по "Условный оператор. Урок 1"

--- src/Main.java
+++ src/Main.java
@@ -65,9 +65,41 @@ public class Main {
         if (passengersWagon < 102){ System.out.println("В вагоне только стоячие места"); }
         else {
             System.out.println("В вагоне нет мест"); }
-
-
-
+//Домашнее задание №3
+        System.out.println("\nДомашнее задание №3\n");
+        // Задание 1
+        int humanAge = 18;
+        if (humanAge >=2 && humanAge <=6) {
+            System.out.println("Если возраст человека равен " + humanAge + " , то ему нужно ходить в детский сад.");
+        }
+        else if ( humanAge >=7 && humanAge <18) {
+            System.out.println("Если возраст человека равен " + humanAge + " , то ему нужно ходить в школу.");
+        }
+         else if (humanAge >=18 && humanAge <24) {
+            System.out.println( "Если возраст человека равен " + humanAge + " , то ему нужно ходить в университет.");
+        } else {
+            System.out.println("Если возраст человека равен " + humanAge + " , то ему нужно ходить на работу");
+        }
+         // Задание 2
+        int ageChild = 15;
+         if (ageChild <= 5) {
+             System.out.println("Ты не можешь кататься на аттракционе!");}
+         else if (ageChild >5 && ageChild <=14) {
+             System.out.println("Ты можешь кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя!");
+         } else {
+             System.out.println("ТЫ можешь кататься без сопровождения взрослого!");
+         }
+         // Задание 3
+        int one = 7;
+         int two = 99;
+         int three = 15;
+         if (one>two && one>three) {
+             System.out.println(one);
+         } else if (two>one && two>three) {
+             System.out.println(two);
+                      }
+         else {
+             System.out.println(three);}
 
 
     }
